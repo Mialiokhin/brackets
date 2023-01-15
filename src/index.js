87e3f66e0b27e9d588845c7108d5ee5931d4a157
Mialiokhin <a.mialiokhin@gmail.com>
2023-01-15 17:59:59 +0300

    refactor: changing the solution to a simpler one

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-    let configString = bracketsConfig.join('').replace(/,/g, ''); // Соединяем все скобки из массива в одну строку
-    let stack = [];// Создаем пустой стек для хранения индексов скобок
-    for (let bracketElement of str) {// Проходим по каждой скобке во входной строке
-        let configIndexElement = configString.indexOf(bracketElement) // Получаем индекс скобки в строке со всеми скобками
-        if (configIndexElement % 2 === 0) {
-            if (bracketElement === configString[configIndexElement + 1] && stack[stack.length - 1] === configIndexElement) {
-                stack.pop();// Проверяем на закрытие для скобок в виде линии которые не проверить по индексу
-            } else {
-                stack.push(configIndexElement) // Если индекс скобки четный - добавляем его в стек
+module.exports =
+    function check(str, bracketsConfig) {
+        let checkStr = str
+        let checkIsLoopFalse = checkStr;
+        let counter = 0;
+        do {
+            for (el of bracketsConfig) {
+                for (let i = 0; i < checkStr.length; i++) {
+                    checkStr = checkStr.replace(`${el[0] + el[1]}`, '');
+                    (checkIsLoopFalse !== checkStr) ? checkIsLoopFalse = checkStr : counter++;
+                }
             }
-        }
-        if (configIndexElement % 2 !== 0) {// Если индекс нечетный -  удаляем закрывающиеся скобки
-            if (stack.pop() !== configIndexElement - 1) {
-                return false;
-            }
-        }
+        } while (checkStr.length !== 0 && counter < 1000)
+        return (checkStr.length === 0)
     }
-    return stack.length === 0 // Если в стеке ничего не осталось - все скобки совпадают
 
-}
 
